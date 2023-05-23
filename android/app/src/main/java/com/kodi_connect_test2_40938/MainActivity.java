a6446101ee0cbf843e663a0179c1eba6796e33f2
Crowdbotics <team@crowdbotics.com>
2023-05-23 22:53:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kodi_connect_test2_40938/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kodi_connect_test2_40938;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "kodi_connect_test2_40938";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kodi-connect-test2-40938",
+  url: "https://kodi-connect-test2-40938.botics.co",
+  api: "https://kodi-connect-test2-40938.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
